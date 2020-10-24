2cf7c31cf4f6f27a57605e83a23fbdc08f95266b
Maija <hello@maijagrudule.com>
2020-10-24 19:14:09 +0200

    Add on hover fly

--- src/components/leaflet/LeafletContainer.vue
+++ src/components/leaflet/LeafletContainer.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     <h1>Leaflet</h1>
-    <div class="relative">
+    <button
+      class="bg-black-900 px-3 py-2 text-white hover:bg-pink-600 mr-2"
+      @mouseenter="flyTo(coord.usa)"
+    >
+      USA
+    </button>
+    <button
+      class="bg-black-900 px-3 py-2 text-white hover:bg-pink-600 pr-2"
+      @mouseenter="flyTo(coord.eu)"
+    >
+      Europe
+    </button>
+    <div class="relative my-5">
       <div id="mapContainer"></div>
     </div>
   </div>
@@ -14,17 +26,26 @@ export default {
   name: "Leaflet",
   data() {
     return {
-      latLong: [52.519325, 13.392709],
-      zoom: 4
+      zoom: 4,
+      map: "",
+      coord: {
+        usa: [40.737, -73.923],
+        eu: [52.519325, 13.392709]
+      }
     };
   },
   methods: {
     setupLeafletMap: function() {
-      const mapDiv = L.map("mapContainer").setView(this.latLong, this.zoom);
+      this.map = L.map("mapContainer", {
+        zoomControl: false
+      }).setView(this.coord.eu, this.zoom);
       L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      }).addTo(mapDiv);
+      }).addTo(this.map);
+    },
+    flyTo: function(coordinates) {
+      this.map.flyTo(new L.LatLng(...coordinates));
     }
   },
   mounted() {
